658909a420fce8e6a40e8b45e3d7e55d61cba160
pratikal1 <chandpratikal1@gmail.com>
2021-07-07 20:07:37 +0530

    Added Header component

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="container max-w-full flex h-24 bg-green-400 justify-center items-center">
+      <h1 className="text-4xl font-bold">Hotels In Banglore</h1>
+    </div>
+  );
+};
+
+export default Header;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './Header';
 import formatData from '../helper/formatData';
 
 const Home = ({ combinedData }) => {
   const data = formatData(combinedData);
-
-  return data.map(({ id, name, price }) => <h1 key={id}>{name}</h1>);
+  console.log(data);
+  return <Header />;
+  // return data.map(({ id, name, price }) => <h1 key={id}>{name}</h1>);
 };
 
 export default Home;
